352071d243c93a7b73e30c71efd619c978972f5d
Annas Rusdi Awandari <w4ndaja@gmail.com>
2021-02-24 21:17:33 +0700

    wip

--- resources/views/pages/patient/index.blade.php
+++ resources/views/pages/patient/index.blade.php
@@ -7,7 +7,7 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Daftar Antrian</h1>
-        <span class="alert alert-info">Total <span class="text-info ml-2">{{$patients->total()}}</span></span>
+        <span>Total <span class="text-info ml-2">{{$patients->total()}}</span></span>
         {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
     </div>
 
