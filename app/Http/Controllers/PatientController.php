a7f5f6f18ff097e67bd0dbbaa852ab85e023bdfe
Annas Rusdi Awandari <w4ndaja@gmail.com>
2021-02-24 20:50:15 +0700

    wip

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -18,6 +18,7 @@ class PatientController extends Controller
         $patients = Patient::with('doctor');
         if(request('from') && request('until')){
             $patients = $patients->whereDate('created_at', '>=', request('from'))->whereDate('created_at', '<=', request('until'))->latest()->paginate(10);   
+            $patients->withPath(route('patient.index').'?from='.request('from').'&until='.request('until'));
         }else{
             $patients = $patients->whereDate('created_at', now()->format('Y-m-d'))->latest()->paginate(10);   
         }
