002794fc13e060b21ce02e2b4de1d0f377100024
Annas Rusdi Awandari <w4ndaja@gmail.com>
2021-03-21 20:06:21 +0700

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Pagination\Paginator as PaginationPaginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        PaginationPaginator::useBootstrap();
+        Paginator::useBootstrap();
     }
 }
